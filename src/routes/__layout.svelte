e1a91b4dbc15c11aa1bc04a8fb530935721d9357
Vania Dimova <ivankadimov8@gmail.com>
2021-10-17 18:54:53 -0400

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+    import '../tailwind.css'
+  import Header from '../components/Header.svelte'
+  </script>
+  
+<Header />
+  <div class="w-full min-h-screen bg-gradient-to-r from-yellow-200 via-pink-300 to-purple-300 px-10 md:px-20 lg:px-52 py-20">
+    <slot></slot>
+  </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+ import {goto} from '$app/navigation';   
+ var searched = '';
+ const search = () => {
+    goto(`${searched}`)
+ }
+</script>
+
+<section>
+    <div class="flex flex-col items-center justify-center">
+        <div class="text-5xl text-center text-white font-semibold mb-12">Type Artist Name?</div>
+        <div class="w-full flex rounded-md bg-white bg-opacity-20 p-1 border border-white border-opacity-30" >
+            <input class="w-4/5 bg-transparent text-darkgray" type="text" bind:value={searched}>
+            <button class="w-1/5 p-2 rounded-md bg-white font-medium" on:click={search}>Search</button>
+        </div>
+       
+    </div>
+</section>
